420488682c59401037fe1509d141086cd6002107
Ankit Arora <ankit.j.arora@gmail.com>
2024-07-04 10:38:31 +0530

    Add /test endpoint and environment variable usage in main.go
    The /test endpoint was added in the main.go file to make a GET request to the address specified by the TEST_URL environment variable. Extra packages were also imported to handle the HTTP request, error messaging, and environment variable handling.

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
 	r := gin.Default()
@@ -8,6 +14,26 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/test", func(c *gin.Context) {
+		testUrl := os.Getenv("TEST_URL")
+		resp, err := http.Get(testUrl)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": fmt.Sprintf("error: %s", err.Error()),
+			})
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": fmt.Sprintf("error: %s", err.Error()),
+			})
+			return
+		}
+		c.String(200, string(body))
 	})
 	r.Run(":8080")
 }
