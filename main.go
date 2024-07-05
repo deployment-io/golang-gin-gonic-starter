766ac437282f9d76ee9756815fdd8953ab771426
Ankit Arora <ankit.j.arora@gmail.com>
2024-07-05 14:39:59 +0530

    Update response message in ping route
    Updated the message returned by the /ping endpoint to better reflect its source. The new message now includes the detail that it is being served from an auto-deployed private service.

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong from private service",
+			"message": "pong from private service auto deployed",
 		})
 	})
 	r.GET("/test", func(c *gin.Context) {
